ad04b14de2a888255359859d8450b8e9ea09d4d7
Remus <37708473+Remus497@users.noreply.github.com>
2026-01-17 17:05:40 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
 
 function App() {
   return (
