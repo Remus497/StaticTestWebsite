cbd623a51aaaf878464d4e0596343714d69f752e
Remus <37708473+Remus497@users.noreply.github.com>
2026-01-17 16:55:38 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,15 +21,6 @@ function App() {
         Test Link
       </a>
       <br />
-      <div>
-        <form action="/action_page.php">
-          <label for="fname">First name:</label><br />
-          <input type="text" id="fname" name="fname" value="John"><br />
-          <label for="lname">Last name:</label><br />
-          <input type="text" id="lname" name="lname" value="Doe"><br /><br />
-          <input type="submit" value="Submit">
-        </form> 
-      </div>
     </main>
   );
 }
