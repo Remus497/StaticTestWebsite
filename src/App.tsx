620ee609e28e54041dbf23a8df9844a1f9fbbc33
Remus <37708473+Remus497@users.noreply.github.com>
2026-01-17 19:00:45 +0000

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,14 @@
 
-import React, { useState } from "react";
-
-import { useForm } from "./useForm";
-
 function App() {
-    // defining the initial state for the form
-    const initialState = {
-        email: "",
-        password: "",
-    };
-
-    // getting the event handlers from our custom hook
-    const { onChange, onSubmit, values } = useForm(
-        loginUserCallback,
-        initialState
-    );
-
-    // a submit function that will execute upon form submission
-    async function loginUserCallback() {
-        // send "values" to database
-    }
-
-    return (
-        // don't mind this ugly form :P
-      <main>
-        skibidi
+  return (
+    <main>
+      skibidi
       <br />
-        <a href = "https://www.youtube.com/watch?v=uhA1hmIuFPk">
-          Test Link
-        </a>
-      </main>
-        <form onSubmit={onSubmit}>
-        <div>
-            <input
-                name='email'
-                id='email'
-                type='email'
-                placeholder='Email'
-                onChange={onChange}
-                required
-                />
-
-            <input
-                name='password'
-                id='password'
-                type='password'
-                placeholder='Password'
-                onChange={onChange}
-                required
-                />
-            <button type='submit'>Login</button>
-        </div>
-        </form>
-    );
+      <a href = "https://www.youtube.com/watch?v=uhA1hmIuFPk">
+        Test Link
+      </a>
+    </main>
+  );
 }
 
 export default App;
