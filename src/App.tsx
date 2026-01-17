b5cfd20b12be81c9293b87b6893dabdb6b456c75
Remus <37708473+Remus497@users.noreply.github.com>
2026-01-17 18:56:42 +0000

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,350 +1,58 @@
 
+import React, { useState } from "react";
+
+import { useForm } from "./useForm";
+
 function App() {
-  return (
-    <main>
-      skibidi
-      <br />
-      <a href = "https://www.youtube.com/watch?v=uhA1hmIuFPk">
-        Test Link
-      </a>
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-      <br />
-      run off the page
-    </main>
-  );
+    // defining the initial state for the form
+    const initialState = {
+        email: "",
+        password: "",
+    };
+
+    // getting the event handlers from our custom hook
+    const { onChange, onSubmit, values } = useForm(
+        loginUserCallback,
+        initialState
+    );
+
+    // a submit function that will execute upon form submission
+    async function loginUserCallback() {
+        // send "values" to database
+    }
+
+    return (
+        // don't mind this ugly form :P
+      <main>
+        skibidi
+      <br />
+        <a href = "https://www.youtube.com/watch?v=uhA1hmIuFPk">
+          Test Link
+        </a>
+      </main>
+        <form onSubmit={onSubmit}>
+        <div>
+            <input
+                name='email'
+                id='email'
+                type='email'
+                placeholder='Email'
+                onChange={onChange}
+                required
+                />
+
+            <input
+                name='password'
+                id='password'
+                type='password'
+                placeholder='Password'
+                onChange={onChange}
+                required
+                />
+            <button type='submit'>Login</button>
+        </div>
+        </form>
+    );
 }
 
 export default App;
