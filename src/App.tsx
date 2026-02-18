cf8778eebc552ecffcb1eb0ff601720eaf641b73
Remus <remus.glh@gmail.com>
2026-02-18 21:21:04 +0000

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 
 function App() {
+
+  function Test_function() {
+    content: window.prompt("Todo content");
+  }
+
   return (
     <main>
+      <button onClick={Test_function}>+ new</button>
       skibidi
       <br />
       <a href = "https://www.youtube.com/watch?v=uhA1hmIuFPk">
